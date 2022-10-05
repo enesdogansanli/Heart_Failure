bdac73c2e8bb6b377b02ea5543f7e8124aa01439
Enes Doğan <enesdogansanli@gmail.com>
2022-10-05 19:48:17 +0300

    data_prep func added.

--- main.py
+++ main.py
@@ -10,17 +10,13 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import StandardScaler
 from xgboost import XGBClassifier
 from lightgbm import LGBMClassifier
+import joblib
 
 pd.set_option('display.max_columns', None)  # Show all columns
 pd.set_option('display.max_rows', None)  # Show all raws
 pd.set_option('display.float_format', lambda x: '%.3f' %
               x)
 pd.set_option('display.width', 500)
-
-df = pd.read_csv(
-    "dataset\heart_failure_clinical_records_dataset.csv")
-
-df.columns = [col.upper() for col in df.columns]
 
 
 def check_df(dataframe, head=5):
@@ -39,14 +35,6 @@ def check_df(dataframe, head=5):
     print(dataframe.isnull().sum())
     print("##################### Quantiles #####################")
     print(dataframe.quantile([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
-
-
-check_df(df)
-
-# Shows the correlation between the target variable and all variables.
-plt.figure(figsize=(10, 10))
-sns.heatmap(df.corr(), vmin=-1, cmap='coolwarm', annot=True)
-plt.show()
 
 
 def cat_summary(dataframe, col_name, plot=False):
@@ -154,23 +142,6 @@ def grab_col_names(dataframe, cat_th=10, car_th=20):
     return cat_cols, num_cols, cat_but_car
 
 
-cat_cols, num_cols, cat_but_car = grab_col_names(df, cat_th=5, car_th=20)
-
-
-for col in cat_cols:
-    cat_summary(df, col)
-
-
-df[num_cols].describe().T
-
-
-correlation_matrix(df, num_cols)
-
-
-for col in num_cols:
-    target_summary_with_num(df, "DEATH_EVENT", col)
-
-
 def outlier_thresholds(dataframe, col_name, q1=0.25, q3=0.75):
     quartile1 = dataframe[col_name].quantile(q1)
     quartile3 = dataframe[col_name].quantile(q3)
@@ -200,11 +171,27 @@ def one_hot_encoder(dataframe, categorical_cols, drop_first=False):
     return dataframe
 
 
-X_scaled = StandardScaler().fit_transform(df[num_cols])
-df[num_cols] = pd.DataFrame(X_scaled, columns=df[num_cols].columns)
+def diabetes_data_prep(dataframe):
+    dataframe.columns = [col.upper() for col in dataframe.columns]
 
-y = df["DEATH_EVENT"]
-X = df.drop(["DEATH_EVENT"], axis=1)
+    cat_cols, num_cols, cat_but_car = grab_col_names(
+        dataframe, cat_th=5, car_th=20)
+
+    cat_cols = [col for col in cat_cols if "DEATH_EVENT" not in col]
+
+    df = one_hot_encoder(dataframe, cat_cols, drop_first=True)
+
+    cat_cols, num_cols, cat_but_car = grab_col_names(df, cat_th=5, car_th=20)
+
+    # replace_with_thresholds(df, "INSULIN")
+
+    X_scaled = StandardScaler().fit_transform(df[num_cols])
+    df[num_cols] = pd.DataFrame(X_scaled, columns=df[num_cols].columns)
+
+    y = df["DEATH_EVENT"]
+    X = df.drop(["DEATH_EVENT"], axis=1)
+
+    return X, y
 
 
 def base_models(X, y, scoring="roc_auc"):
@@ -226,4 +213,77 @@ def base_models(X, y, scoring="roc_auc"):
             f"{scoring}: {round(cv_results['test_score'].mean(), 4)} ({name}) ")
 
 
-base_models(X, y, scoring="accuracy")
+knn_params = {"n_neighbors": range(2, 50)}
+
+cart_params = {'max_depth': range(1, 20),
+               "min_samples_split": range(2, 30)}
+
+rf_params = {"max_depth": [8, 15, None],
+             "max_features": [5, 7, "auto"],
+             "min_samples_split": [15, 20],
+             "n_estimators": [200, 300]}
+
+xgboost_params = {"learning_rate": [0.1, 0.01],
+                  "max_depth": [5, 8],
+                  "n_estimators": [100, 200],
+                  "colsample_bytree": [0.5, 1]}
+
+lightgbm_params = {"learning_rate": [0.01, 0.1],
+                   "n_estimators": [300, 500],
+                   "colsample_bytree": [0.7, 1]}
+
+classifiers = [('KNN', KNeighborsClassifier(), knn_params),
+               ("CART", DecisionTreeClassifier(), cart_params),
+               ("RF", RandomForestClassifier(), rf_params),
+               ('XGBoost', XGBClassifier(use_label_encoder=False,
+                eval_metric='logloss'), xgboost_params),
+               ('LightGBM', LGBMClassifier(), lightgbm_params)]
+
+
+def hyperparameter_optimization(X, y, cv=3, scoring="roc_auc"):
+    print("Hyperparameter Optimization....")
+    best_models = {}
+    for name, classifier, params in classifiers:
+        print(f"########## {name} ##########")
+        cv_results = cross_validate(classifier, X, y, cv=cv, scoring=scoring)
+        print(
+            f"{scoring} (Before): {round(cv_results['test_score'].mean(), 4)}")
+
+        gs_best = GridSearchCV(classifier, params, cv=cv,
+                               n_jobs=-1, verbose=False).fit(X, y)
+        final_model = classifier.set_params(**gs_best.best_params_)
+
+        cv_results = cross_validate(final_model, X, y, cv=cv, scoring=scoring)
+        print(
+            f"{scoring} (After): {round(cv_results['test_score'].mean(), 4)}")
+        print(f"{name} best params: {gs_best.best_params_}", end="\n\n")
+        best_models[name] = final_model
+    return best_models
+
+
+def voting_classifier(best_models, X, y):
+    print("Voting Classifier...")
+    voting_clf = VotingClassifier(estimators=[('KNN', best_models["KNN"]), ('RF', best_models["RF"]),
+                                              ('LightGBM', best_models["LightGBM"])],
+                                  voting='soft').fit(X, y)
+    cv_results = cross_validate(voting_clf, X, y, cv=3, scoring=[
+                                "accuracy", "f1", "roc_auc"])
+    print(f"Accuracy: {cv_results['test_accuracy'].mean()}")
+    print(f"F1Score: {cv_results['test_f1'].mean()}")
+    print(f"ROC_AUC: {cv_results['test_roc_auc'].mean()}")
+    return voting_clf
+
+
+def main():
+    df = pd.read_csv("dataset\heart_failure_clinical_records_dataset.csv")
+    X, y = diabetes_data_prep(df)
+    base_models(X, y)
+    best_models = hyperparameter_optimization(X, y)
+    voting_clf = voting_classifier(best_models, X, y)
+    joblib.dump(voting_clf, "voting_clf.pkl")
+    return voting_clf
+
+
+if __name__ == "__main__":
+    print("The program has started.")
+    main()
